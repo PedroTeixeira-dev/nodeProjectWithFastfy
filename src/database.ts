7c30d3242e66aa5b431e707c2a916004c4db2232
Pedro Teixeira <pedro15at@gmail.com>
2025-08-13 18:53:39 -0300

    working on the knex data base

--- src/database.ts
+++ src/database.ts
@@ -6,10 +6,10 @@ export const config: Knex.Config = {
 		filename: './db/app.db'
 	},
 	useNullAsDefault: true,
-    migrations: {
-        extension: 'ts',
-        directory: './db/migrations'
-    }
+	migrations: {
+		extension: 'ts',
+		directory: './db/migrations'
+	}
 };
 
 export const knex = setupKnex(config);
